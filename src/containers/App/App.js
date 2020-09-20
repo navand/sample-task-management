d2f31c92792872bef909f47ed7b4385ced0c3770
Navand Namiranian <navandn@gmail.com>
2020-09-20 17:08:17 +0430

    Initialize commit, project structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
+import './App.css';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  return <div className="App"></div>;
+};
+
+export default App;
